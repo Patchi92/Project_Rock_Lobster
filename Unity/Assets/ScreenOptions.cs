f17a73fb947b2ab49fb111c2feb01666b91ed45a
Patchi92 <lechieu@hotmail.com>
2014-11-21 16:52:59 +0100

    Screen setup

---
+++ Unity/Assets/ScreenOptions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(1024, 768, true, 60);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
